2674174bcd3f5cc19e9ae650d63cb309b8e9f848
juissi999 <jussi.kaatiala@gmail.com>
2023-09-03 01:43:36 +0300

    Add file copying flag + functionality to app

--- psync.py
+++ psync.py
@@ -1,13 +1,18 @@
 import argparse
 
-from utils import find_files_in_folderstructure
+from utils import find_files_in_folderstructure, copy_files
 
 parser = argparse.ArgumentParser("simple_example")
 
 parser.add_argument("inputdir", help="Input directory.", type=str)
 parser.add_argument("outputdir", help="Output directory.", type=str)
 parser.add_argument('-v', '--verbose', action='store_true')  # on/off flag
+parser.add_argument('-s', '--sync', action='store_true')  # on/off flag
 
+# omit duplicate hashes
+# omit duplicate filenames
+# rename duplicate filenames
+# flatten source file structure
 
 args = parser.parse_args()
 
@@ -31,9 +36,12 @@ hashes_in_inputdir = [x[2] for x in files_in_inputdirectory]
 hashes_in_outputdir = {x[2] for x in files_in_outputdirectory}
 
 intersecting_hashes = []
+non_intersecting_hashes = []
 for index, hashvalue in enumerate(hashes_in_inputdir):
     if (hashvalue in hashes_in_outputdir):
         intersecting_hashes.append(index)
+    else:
+        non_intersecting_hashes.append(index)
 
 if len(intersecting_hashes) > 0:
     print("Some files in the input directory seem to exist in the output directory already.")
@@ -41,3 +49,12 @@ if len(intersecting_hashes) > 0:
     if args.verbose:
       print("Files:")
       print([filenames_in_inputdir[index] for index in intersecting_hashes])
+
+files_to_copy = [(files_in_inputdirectory[index]) for index in non_intersecting_hashes]
+
+if (args.sync):
+  print("Copying " + str(len(files_to_copy)) + " files...")
+  copy_files(files_to_copy, args.inputdir, args.outputdir)
+  print("Done.")
+else:
+   print("About to copy " + str(len(files_to_copy)) + " files to destination dir. Please add -s flag to copy.")
