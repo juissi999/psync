47f29253aaae36b76ef744f6240a38fdb42334ae
juissi999 <jussi.kaatiala@gmail.com>
2023-09-03 00:46:30 +0300

    Add verbose mode

--- psync.py
+++ psync.py
@@ -6,13 +6,10 @@ parser = argparse.ArgumentParser("simple_example")
 
 parser.add_argument("inputdir", help="Input directory.", type=str)
 parser.add_argument("outputdir", help="Output directory.", type=str)
+parser.add_argument('-v', '--verbose', action='store_true')  # on/off flag
 
 
 args = parser.parse_args()
-
-print("Input directory: " + args.inputdir)
-print("Output directory: " + args.outputdir)
-
 
 files_in_inputdirectory = find_files_in_folderstructure(args.inputdir)
 files_in_outputdirectory = find_files_in_folderstructure(args.outputdir)
@@ -20,10 +17,15 @@ files_in_outputdirectory = find_files_in_folderstructure(args.outputdir)
 filenames_in_inputdir = [x[1] for x in files_in_inputdirectory]
 filenames_in_outputdir = [x[1] for x in files_in_outputdirectory]
 
-print("FILES IN INPUT-DIR:")
-print(filenames_in_inputdir)
-print("FILES IN OUTPUT-DIR:")
-print(filenames_in_outputdir)
+print("Input directory: " + args.inputdir + " (" + str(len(filenames_in_inputdir)) + " files)")
+print("Output directory: " + args.outputdir + " (" + str(len(filenames_in_outputdir)) + " files)")
+
+
+if args.verbose:
+  print("FILES IN INPUT-DIR:")
+  print(filenames_in_inputdir)
+  print("FILES IN OUTPUT-DIR:")
+  print(filenames_in_outputdir)
 
 hashes_in_inputdir = [x[2] for x in files_in_inputdirectory]
 hashes_in_outputdir = {x[2] for x in files_in_outputdirectory}
@@ -35,5 +37,7 @@ for index, hashvalue in enumerate(hashes_in_inputdir):
 
 if len(intersecting_hashes) > 0:
     print("Some files in the input directory seem to exist in the output directory already.")
-    print("Files:")
-    print([filenames_in_inputdir[index] for index in intersecting_hashes])
+
+    if args.verbose:
+      print("Files:")
+      print([filenames_in_inputdir[index] for index in intersecting_hashes])
