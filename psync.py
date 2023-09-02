1833f597e1daddd7b468e7880e7cde83eb9ec65b
juissi999 <jussi.kaatiala@gmail.com>
2023-09-03 00:41:16 +0300

    Add input/output hash comparison

--- psync.py
+++ psync.py
@@ -20,6 +20,20 @@ files_in_outputdirectory = find_files_in_folderstructure(args.outputdir)
 filenames_in_inputdir = [x[1] for x in files_in_inputdirectory]
 filenames_in_outputdir = [x[1] for x in files_in_outputdirectory]
 
+print("FILES IN INPUT-DIR:")
 print(filenames_in_inputdir)
-print(filenames_in_inputdir)
+print("FILES IN OUTPUT-DIR:")
+print(filenames_in_outputdir)
 
+hashes_in_inputdir = [x[2] for x in files_in_inputdirectory]
+hashes_in_outputdir = {x[2] for x in files_in_outputdirectory}
+
+intersecting_hashes = []
+for index, hashvalue in enumerate(hashes_in_inputdir):
+    if (hashvalue in hashes_in_outputdir):
+        intersecting_hashes.append(index)
+
+if len(intersecting_hashes) > 0:
+    print("Some files in the input directory seem to exist in the output directory already.")
+    print("Files:")
+    print([filenames_in_inputdir[index] for index in intersecting_hashes])
